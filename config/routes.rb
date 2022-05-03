0b46675c57603bc757501d78bb217bec1a78c1f1
takaheraw <takaheraw@gmail.com>
2022-05-04 07:53:34 +0900

    猫でもわかるHotwire入門 Turbo編

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :owls
+  resources :hedgehogs
+  resources :chicks
+  resources :dogs
+  resources :cats
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "cats#index"
+end
